279d5fe575e318f23543f8878348d4c1bd6419cc
yoko345 <spblackhole89.gmail.com>
2024-04-13 21:52:17 +0900

    設定ファイルの設定

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -70,12 +70,12 @@ class Filters extends BaseFilters
     public array $globals = [
         'before' => [
             // 'honeypot',
-            // 'csrf',
-            // 'invalidchars',
+            'csrf',
+            'invalidchars',
         ],
         'after' => [
             // 'honeypot',
-            // 'secureheaders',
+            'secureheaders',
         ],
     ];
 
