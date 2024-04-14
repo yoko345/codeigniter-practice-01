5bfb7b00138ef4d1b9b1d4e682ffcc271a09a18b
yoko345 <spblackhole89.gmail.com>
2024-04-14 15:16:09 +0900

    データベースにnewsを追加する

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,6 +10,8 @@ use App\Controllers\Pages;
 $routes->get('/', 'Home::index');
 
 $routes->get('news', [News::class, "index"]);
+$routes->get('news/new', [News::class, "new"]);
+$routes->post('news', [News::class, "create"]);
 $routes->get('news/(:segment)', [News::class, "view"]);
 
 $routes->get('pages', [Pages::class, "index"]);
--- app/Controllers/News.php
+++ app/Controllers/News.php
@@ -33,4 +33,41 @@ class News extends Controller
             . view("news/view", $data)
             . view("templates/footer", $data);
     }
+
+    public function new()
+    {
+        helper("form");
+
+        return view("templates/header", ["title" => "Create a news item"])
+            . view("news/create")
+            . view("templates/footer");
+    }
+
+    public function create()
+    {
+        helper("form");
+
+        $data = $this->request->getPost(["title", "body"]);
+
+        if (!$this->validateData($data, [
+            "title" => "required|max_length[255]|min_length[3]",
+            "body" => "required|max_length[5000]|min_length[10]",
+        ])) {
+            return $this->new();
+        }
+
+        $post = $this->validator->getValidated();
+
+        $model = model(NewsModel::class);
+
+        $model->save([
+            "title" => $post["title"],
+            "slug" => url_title($post["title"], "-", true),
+            "body" => $post["body"],
+        ]);
+
+        return view("templates/header", ["title" => "Create a news item"])
+            . view("news/success")
+            . view("templates/footer");
+    }
 }
